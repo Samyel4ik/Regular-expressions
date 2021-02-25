d747abb94aed10ec3c3340f202aaad3da30f84d3
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-02-25 19:49:35 +0300

    T1

--- src/Task1/Expression.java
+++ src/Task1/Expression.java
@@ -17,10 +17,10 @@ public class Expression {
 
         int method = scanner.nextInt();
         if (method == 1) {
-            System.out.println(sortedTextBySentences(text));
+            System.out.println(createText(text));
         }
         if (method == 2) {
-            System.out.println(sortedText(text));
+            System.out.println(sortedTextByWordsInSentences(text));
         }
         if (method == 3) {
             Scanner scanner1 = new Scanner(System.in);
@@ -46,8 +46,7 @@ public class Expression {
     }
 
     public static String[] splitByParagraphs(String text) {                 //делим текст на абзацы.
-        String[] paragraph = text.split("\n");
-        return paragraph;
+        return text.split("\n");
     }
 
     public static String[] sortByNumOfWords(String[] paragraph) {      //  сортировка абзцев по кол предлож
@@ -68,42 +67,57 @@ public class Expression {
         return paragraph;
     }
 
-    public static String sortedTextBySentences(String text) {
+    public static String[] sortedTextBySentences(String text) {
         String[] paragraph = splitByParagraphs(text);
-        String[] sortedParagraph = sortByNumOfWords(paragraph);
+        return sortByNumOfWords(paragraph);
+    }
+
+    public static String createText(String text) {
+        String[] sortedParagraph = sortedTextBySentences(text);
 
         StringBuilder sorted = new StringBuilder();                     // перезаписываем
         for (int i = 0; i < sortedParagraph.length; i++) {
-            sorted.append(paragraph[i]).append("\n");
+            sorted.append(sortedParagraph[i]).append("\n");
         }
         return sorted.toString();
 
     }
 
+
     //2.
-    public static String[] divideIntoWords(String text) {       // сортировка слов в предложении по количеству букв.
-        String text1 = text.replace("\n", "");// удаляем с предложения все переносы
-        String[] array = text1.split(" ");              // разделяем  предложение на слова
+    public static String[] divideIntoParagraphs(String text) {
+        String[] array = text.split("\n");                 //разделяем текст на абзацы
         return array;
     }
 
-    public static String[] sortingWords(String[] array) {           //в предложении произвели сортировку
+    public static String[] divideIntoSentencesM(String text) {
+        String[] array = text.split(".");                   // разделяем на предложения
+        return array;
+    }
+
+    public static String[] divideIntoWords(String text) {
+        String[] arrayWord = text.split(" ");                  // разделяем  предложение на слова
+        return arrayWord;
+    }
+
+    public static String[] sortingWords(String[] arrayWord) {           //в предложении произвели сортировку
 
         boolean arraySorted = false;
         while (!arraySorted) {
             arraySorted = true;
-            for (int i = 0; i < array.length - 1; i++) {
-                if (array[i].length() > array[i + 1].length()) {
-                    String zamena = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = zamena;
+            for (int i = 0; i < arrayWord.length - 1; i++) {
+                if (arrayWord[i].length() > arrayWord[i + 1].length()) {
+                    String zamena = arrayWord[i];
+                    arrayWord[i] = arrayWord[i + 1];
+                    arrayWord[i + 1] = zamena;
                     arraySorted = false;
                 }
             }
         }
-        return array;
+        return arrayWord;
     }
-    public static String line (String []arrayWords) {
+
+    public static String buildSentence(String[] arrayWords) {
         StringBuilder sorted = new StringBuilder();
         for (int i = 0; i < arrayWords.length; i++) {
             sorted.append(arrayWords[i]).append(" ");
@@ -111,18 +125,23 @@ public class Expression {
         return sorted.toString();
     }
 
-    public static String sortedText(String text) {
-        String[]weDivideIntoSentences= text.split("\\.");                                // разбиваем на предложения
-        StringBuilder sortedSentenceResult = new StringBuilder();
+    public static String sortedTextByWordsInSentences(String text) {
+        String[] divideIntoParagraphs = divideIntoParagraphs(text);                                // разбиваем на aбзацы
 
-        for (int i = 0; i < weDivideIntoSentences.length; i++) {                                // взяли предложение
-            String [] divideIntoWords = divideIntoWords(weDivideIntoSentences[i]);              // убрали лишнее и разбили на слова
-            String []sortingWords = sortingWords(divideIntoWords);                               // сорт слова в предложении
+        StringBuilder sortParagraphByWordLength = new StringBuilder();
+        StringBuilder sortedTextByWordsInSentences = new StringBuilder();
 
-            sortedSentenceResult.append(line(sortingWords)).append(".").append("\n");
+        for (int i = 0; i < divideIntoParagraphs.length; i++) {
+            String[] divideIntoSentencesM = divideIntoSentencesM(divideIntoParagraphs[i]);                      // разбиваем абзац на предложения
+            String[] divideIntoWords = divideIntoWords(String.valueOf(divideIntoSentencesM));                  // разбиваем предложение  на слова
+            String[] sortingWords = sortingWords(divideIntoWords);                                           // отсортировали слова  в предлож
+            String buildSentence = buildSentence(sortingWords);                                              // собрали предложение
+            String textResult = sortParagraphByWordLength.append(buildSentence).append(".").toString();     //собрали абзац
+            sortedTextByWordsInSentences.append(textResult).append("\n");
         }
-        return sortedSentenceResult.toString();
+        return sortedTextByWordsInSentences.toString();
     }
+
 
     //  3.отсортировать слова по количеству заданаго символа , в предложении(по убыванию),елси символов одинаково по алфавиту.
     public static int numWord(String str, char x) {
@@ -135,44 +154,47 @@ public class Expression {
         }
         return t;                                                                           //количество симолов в слове +
     }
-    // метод сравнивания двух слов на первую букву в алфавите --
 
-    public static String sortedSentenceWord(String text, char x) {                           //сортируем предложение по количуству символов в слове
-        String[] arrayPredlozenie = text.split(" ");
+    public static String[] sortedSentenceWord(String[] arrayWords, char x) {               //сортируем предложение по количуству символов в слове
         boolean arraySorted = false;
         while (!arraySorted) {
             arraySorted = true;
 
-            for (int i = 0; i < arrayPredlozenie.length - 1; i++) {
-                if (numWord(arrayPredlozenie[i], x) < numWord(arrayPredlozenie[i + 1], x)) {
-                    String zamena = arrayPredlozenie[i];
-                    arrayPredlozenie[i] = arrayPredlozenie[i + 1];
-                    arrayPredlozenie[i + 1] = zamena;
+            for (int i = 0; i < arrayWords.length - 1; i++) {
+                if (numWord(arrayWords[i], x) < numWord(arrayWords[i + 1], x)) {
+                    String zamena = arrayWords[i];
+                    arrayWords[i] = arrayWords[i + 1];
+                    arrayWords[i + 1] = zamena;
                     arraySorted = false;
-                }
-                if (numWord(arrayPredlozenie[i], x) == numWord(arrayPredlozenie[i + 1], x)) {
-                    //сортировка слов по алфавиту
-
                 }
             }
         }
+        return arrayWords;
+    }
+
+    public static String sortedSentence(String[] arrayWords) {
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < arrayPredlozenie.length; i++) {
-            result.append(arrayPredlozenie[i]).append(" ");
+        for (int i = 0; i < arrayWords.length; i++) {
+            result.append(arrayWords[i]).append(" ");
         }
         return result.toString();
     }
 
     public static String resultSortedText(String text, char x) {
-        String textNew = text.replace("\n", "");
-
-        String[] arrayNew = textNew.split("\\.");
+        String[] arrayParagraph = divideIntoParagraphs(text);                               //абзац
+        StringBuilder result1 = new StringBuilder();
         StringBuilder resultTextSorted = new StringBuilder();
 
-        for (int i = 0; i < arrayNew.length; i++) {
-            resultTextSorted.append(sortedSentenceWord(arrayNew[i], x)).append(".").append("\n");
+        for (int i = 0; i < arrayParagraph.length; i++) {
+            String[] divideIntoSentencesM = divideIntoSentencesM(arrayParagraph[i]);       //предложение
+            String[] divideIntoWords = divideIntoWords(String.valueOf(divideIntoSentencesM)); // слово
+            String[] sortedSentenceWord = sortedSentenceWord(divideIntoWords, x);            // сортировка
+            String sortedSentence = sortedSentence(sortedSentenceWord);                      //склеили предложение
+            String text1 = result1.append(sortedSentence).append(".").toString();
+            resultTextSorted.append(text1).append("\n");
         }
+
         return resultTextSorted.toString();
     }
 }
