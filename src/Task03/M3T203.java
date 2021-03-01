b2933b6de34358aa0778a508284dc22eb89ec3e0
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-03-01 18:16:08 +0300

    T03

---
+++ src/Task03/M3T203.java
@@ -0,0 +1,38 @@
+package Task03;
+
+public class M3T203 {
+    //Даны открывающиеся и закрывающиеся тэги. Вывести в консоль их имена и тип
+    public static void main(String[] args) {
+        String string = "<head><body/><child><head/>";
+        StringBuilder result = new StringBuilder();
+        char[] array = string.toCharArray();
+
+        boolean tagFound = false;
+        String str = "";
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] == '<') {
+                tagFound = true;
+                continue;
+            }
+            if (array[i] == '>') {
+                System.out.println("открывающий тэг:"+ str);
+                str = "";
+                tagFound = false;
+
+
+            }
+            if (array[i] == '/') {
+                System.out.println("закрывающий тэг:"+ str);
+                str="";
+                tagFound = false;
+
+            }
+            if (tagFound) {
+                str += array[i];
+            }
+        }
+    }
+}
+
